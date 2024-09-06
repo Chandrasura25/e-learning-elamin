b397ae8eff44a3bdbe9641fbe0c4854e49aeef63
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2024-09-06 11:13:52 +0100

    e learning

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,24 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <App />
+      <ToastContainer
+        position="bottom-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
     </BrowserRouter>
   </StrictMode>
 );
