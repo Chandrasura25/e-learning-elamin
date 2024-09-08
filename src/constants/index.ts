ea09466ab05836976d5a74e08baa35d05eae5a7a
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2024-09-08 03:58:06 +0100

    updates

---
+++ src/constants/index.ts
@@ -0,0 +1,70 @@
+export const sidebarLinks = [
+  {
+    imgURL: "/images/home.svg",
+    route: "/dashboard",
+    label: "Home",
+  },
+  {
+    imgURL: "/images/shuffle.svg",
+    route: "/explore",
+    label: "Explore",
+  },
+  {
+    imgURL: "/images/user.svg",
+    route: "/profile",
+    label: "Profile",
+  },
+  {
+    imgURL: "/images/create.svg",
+    route: "/create-post",
+    label: "Post Confession",
+  },
+  {
+    imgURL: "/images/chat.svg",
+    route: "/chat",
+    label: "Chat",
+  },
+  {
+    imgURL: "/images/community.svg",
+    route: "/rooms",
+    label: "Rooms",
+  },
+  {
+    imgURL: "/images/counsellor.svg",
+    route: "/counsellor",
+    label: "Counsellors",
+  },
+];
+
+export const supervisorSidebarLinks = [
+  {
+    imgURL: "/images/home.svg",
+    route: "/dashboard",
+    label: "Home",
+  },
+  {
+    imgURL: "/images/shuffle.svg",
+    route: "/explore",
+    label: "Explore",
+  },
+  {
+    imgURL: "/images/user.svg",
+    route: "/profile",
+    label: "Profile",
+  },
+  {
+    imgURL: "/images/chat.svg",
+    route: "/chat",
+    label: "Chat",
+  },
+  {
+    imgURL: "/images/community.svg",
+    route: "/rooms",
+    label: "Rooms",
+  },
+  {
+    imgURL: "/images/withdraw.svg",
+    route: "/withdraw",
+    label: "Withdraw",
+  },
+];
