021c27cb6fc1f2be78a0e979b3b2e47e3845d5fe
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2024-09-08 05:00:55 +0100

    change

--- src/components/shared/Topbar.tsx
+++ src/components/shared/Topbar.tsx
@@ -1,9 +1,81 @@
-import React from 'react'
+import {
+  CreditCard,
+  Keyboard,
+  LogOut,
+  Settings,
+  User,
+  UserPlus,
+} from "lucide-react";
 
-const Topbar = () => {
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { useAuth } from "@/contexts/AuthContext";
+import Logo from "./../../assets/logo.png";
+export default function Topbar() {
+  const { logout, user, role } = useAuth();
   return (
-    <div>Topbar</div>
-  )
+    <div className="bg-red-1 p-3 shadow-lg border-b border-gray-600 fixed top-0 z-30 w-full">
+      <div className="flex items-center justify-between">
+        <a href="https://learming.elaminschoolabuja.com" className="flex gap-1">
+          <img src={Logo} className="mr-3 h-10 w-10" alt="El-Amin" />
+          <span className="text-heading3-bold uppercase text-light-1 max-xs:hidden">
+            El-Amin
+          </span>
+        </a>
+        <div className="flex text-white items-center gap-4">
+          <div className="flex items-center gap-2">
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button variant="outline" className="bg-transparent border-none hover:bg-transparent hover:text-light-2">
+                    <UserPlus className="mr-2 h-4 w-4" />
+                    <span>{user?.firstname}</span>
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent className="w-56">
+                <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuGroup>
+                  <DropdownMenuItem>
+                    <User className="mr-2 h-4 w-4" />
+                    <span>Profile</span>
+                    <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
+                  </DropdownMenuItem>
+                  <DropdownMenuItem>
+                    <CreditCard className="mr-2 h-4 w-4" />
+                    <span>Billing</span>
+                    <DropdownMenuShortcut>⌘B</DropdownMenuShortcut>
+                  </DropdownMenuItem>
+                  <DropdownMenuItem>
+                    <Settings className="mr-2 h-4 w-4" />
+                    <span>Settings</span>
+                    <DropdownMenuShortcut>⌘S</DropdownMenuShortcut>
+                  </DropdownMenuItem>
+                  <DropdownMenuItem>
+                    <Keyboard className="mr-2 h-4 w-4" />
+                    <span>Keyboard shortcuts</span>
+                    <DropdownMenuShortcut>⌘K</DropdownMenuShortcut>
+                  </DropdownMenuItem>
+                </DropdownMenuGroup>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem onClick={logout} className="cursor-pointer">
+                  <LogOut className="mr-2 h-4 w-4" />
+                  <span>Log out</span>
+                  <DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut>
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
-
-export default Topbar
