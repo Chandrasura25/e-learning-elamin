fb847d7acbaa2a1957d59c9752a79015ac9a24ca
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2024-09-08 08:47:14 +0100

    e-note

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,8 +1,23 @@
-import React from 'react'
-
+import DashboardTable from '@/components/Tables/DashboardTable';
+import { useAuth } from '@/contexts/AuthContext';
 const Dashboard = () => {
+  const { user } = useAuth();
   return (
-    <div>Dashboard</div>
+    <div>
+      <div className="w-full h-fit py-3 px-8 flex justify-between items-center bg-white rounded-[16px]">
+        <div className="">
+          <h2 className='text-heading2-semibold text-light-4'>Hi {user?.firstname}!</h2>
+          <p>You have completed Lorem, ipsum dolor. Start writing your E-note</p>
+        </div>
+        <div className="">
+          <img src="/images/writing.gif" alt="" className='h-24' />
+        </div>
+        <div className=""></div>
+      </div>
+      <div className="bg-white mt-4 p-4 rounded-[16px]">
+        <DashboardTable />
+      </div>
+    </div>
   )
 }
 
