0299f5e154ed73e9e9e66edff0e5546bb52d8a85
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2024-09-08 03:57:43 +0100

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import { Dash } from "./pages/superadmin/Dash";
 import { useAuth } from "./contexts/AuthContext";
+import Layout from "./pages/Layout";
 
 function App() {
   const { role } = useAuth();
@@ -13,7 +14,9 @@ function App() {
         {role === "superadmin" ? (
           <Route path="/superadmin-dashboard" element={<Dash />} />
         ) : (
-          <Route path="/teacher-dashboard" element={<Dashboard />} />
+          <Route path="/" element={<Layout />}>
+            <Route path="/teacher-dashboard" element={<Dashboard />} />
+          </Route>
         )}
       </Routes>
     </>
