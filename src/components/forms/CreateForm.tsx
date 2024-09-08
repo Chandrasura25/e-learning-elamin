1a5ff3c447ab8c2c71b5f845d54de9f946d88bc5
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2024-09-08 09:00:00 +0100

    creation

---
+++ src/components/forms/CreateForm.tsx
@@ -0,0 +1,9 @@
+import style from "@/styles/login.module.css";
+
+export const CreateForm = () => {
+  return (
+    <div className={style.body}>
+      <div className={`${style.form} w-full`}>CreateForm</div>
+    </div>
+  );
+};
