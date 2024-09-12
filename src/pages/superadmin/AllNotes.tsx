24a222eff44671acfafb2fa2d7e38d570d1434d3
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2024-09-13 00:29:06 +0100

    note

--- src/pages/superadmin/AllNotes.tsx
+++ src/pages/superadmin/AllNotes.tsx
@@ -1,9 +1,61 @@
-import React from 'react'
+import { axiosPrivate } from '@/api/axios';
+import SupervisorTable from '@/components/Tables/SupervisorTable';
+import { useEffect, useState } from 'react';
+import { Watch } from "react-loader-spinner";
 
 const AllNotes = () => {
+  const [notes, setNotes] = useState([]);
+  const [loading, setLoading] = useState(true); 
+
+  const getNotes = async () => {
+    try {
+      if (user?.id) {
+        setLoading(true); 
+        const response = await axiosPrivate.get(`/all-notes`);
+        setNotes(response.data.notes); // Assuming the API returns an object of notes
+      }
+    } catch (error) {
+      console.error("Error fetching notes:", error);
+    } finally {
+      setLoading(false); // Set loading to false once data is fetched or if an error occurs
+    }
+  };
+  useEffect(() => {
+
+    getNotes();
+  }, [user?.id, axiosPrivate]);
+
   return (
-    <div>AllNotes</div>
+    <div>
+      <div className="w-full h-fit py-3 px-8 flex justify-between items-center bg-white rounded-[16px]">
+        <div className="">
+          <h2 className='text-heading2-semibold text-light-4'>Hi {user?.firstname}!</h2>
+          <p>Make sure to jot down a note about today’s lesson and any important details for the students!</p>
+        </div>
+        <div className="">
+          <img src="/images/writing.gif" alt="" className='h-24' />
+        </div>
+        <div className=""></div>
+      </div>
+      <div className="bg-white mt-4 p-4 rounded-[16px]">
+        {loading ? (
+          <div className="flex justify-center items-center h-64">
+            <Watch
+              height="80"
+              width="80"
+              radius="48"
+              color="#B43330"
+              ariaLabel="watch-loading"
+              wrapperStyle={{}}
+              wrapperClass=""
+            />
+          </div>
+        ) : (
+          <SupervisorTable data={notes} getNotes={getNotes} />
+        )}
+      </div>
+    </div>
   )
 }
 
-export default AllNotes
+export default AllNotes;
