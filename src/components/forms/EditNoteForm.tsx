3d4b73a0eff0e038b24eb7f9e57ef9c9a2552e43
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2024-09-12 15:59:11 +0100

    note

--- src/components/forms/EditNoteForm.tsx
+++ src/components/forms/EditNoteForm.tsx
@@ -3,7 +3,11 @@ import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Calendar } from "@/components/ui/calendar";
 import { Calendar as CalendarIcon } from "lucide-react";
-import { Popover, PopoverTrigger, PopoverContent } from "@/components/ui/popover";
+import {
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+} from "@/components/ui/popover";
 import { addDays, format } from "date-fns";
 import { DateRange } from "react-day-picker";
 import { toast } from "react-toastify";
@@ -31,6 +35,16 @@ const EditNoteForm = ({ note }) => {
     };
     getClasses();
   }, []);
+
+  // Preselect the arms when a class is selected on load
+  useEffect(() => {
+    if (selectedClass) {
+      const selectedClassObj = classes.find(
+        (cls) => cls.enrolclass?.id === Number(selectedClass)
+      );
+      setArms(selectedClassObj?.arms || []);
+    }
+  }, [selectedClass, classes]);
 
   const handleClassChange = (e) => {
     const classId = e.target.value;
@@ -81,9 +95,9 @@ const EditNoteForm = ({ note }) => {
     setLoading(true);
     try {
       const response = await axiosPrivate.put(`/note/${note.id}`, data);
-      toast.success("Lesson plan updated successfully!");
+      toast.success(response.data.message);
     } catch (error) {
-      toast.error("Error updating lesson plan. Please try again.");
+      toast.error(error.response.data.message);
     } finally {
       setLoading(false);
     }
@@ -101,31 +115,41 @@ const EditNoteForm = ({ note }) => {
           placeholder="e.g., Discussion, Lecture, Hands-on"
         />
         {errors.steps?.[step]?.mode && (
-          <p className="text-red-600 text-sm">{errors.steps[step]?.mode?.message}</p>
+          <p className="text-red-600 text-sm">
+            {errors.steps[step]?.mode?.message}
+          </p>
         )}
       </div>
 
       <div className="mt-4">
-        <label className="block text-sm font-medium text-gray-700">Teacher's Activities</label>
+        <label className="block text-sm font-medium text-gray-700">
+          Teacher's Activities
+        </label>
         <textarea
           {...register(`steps.${step}.teacherActivities.0`)}
           className="mt-1 block w-full p-2 border rounded-md"
           placeholder="Enter teacher's activities"
         />
         {errors.steps?.[step]?.teacherActivities?.[0] && (
-          <p className="text-red-600 text-sm">{errors.steps[step]?.teacherActivities[0]?.message}</p>
+          <p className="text-red-600 text-sm">
+            {errors.steps[step]?.teacherActivities[0]?.message}
+          </p>
         )}
       </div>
 
       <div className="mt-4">
-        <label className="block text-sm font-medium text-gray-700">Students' Activities</label>
+        <label className="block text-sm font-medium text-gray-700">
+          Students' Activities
+        </label>
         <textarea
           {...register(`steps.${step}.studentActivities.0`)}
           className="mt-1 block w-full p-2 border rounded-md"
           placeholder="Enter students' activities"
         />
         {errors.steps?.[step]?.studentActivities?.[0] && (
-          <p className="text-red-600 text-sm">{errors.steps[step]?.studentActivities[0]?.message}</p>
+          <p className="text-red-600 text-sm">
+            {errors.steps[step]?.studentActivities[0]?.message}
+          </p>
         )}
       </div>
     </div>
@@ -138,18 +162,24 @@ const EditNoteForm = ({ note }) => {
       <form onSubmit={handleSubmit(onSubmit)} className="space-y-6">
         <div className="flex sm:flex-row flex-col sm:space-x-4">
           <div className="sm:flex-1">
-            <label className="block text-sm font-medium text-gray-700">Week</label>
+            <label className="block text-sm font-medium text-gray-700">
+              Week
+            </label>
             <input
               type="text"
               {...register("week")}
               className="mt-1 block w-full p-2 border rounded-md"
               placeholder="Week 1"
             />
-            {errors.week && <p className="text-red-600 text-sm">{errors.week.message}</p>}
+            {errors.week && (
+              <p className="text-red-600 text-sm">{errors.week.message}</p>
+            )}
           </div>
 
           <div className="sm:flex-1">
-            <label className="block text-sm font-medium text-gray-700">Date</label>
+            <label className="block text-sm font-medium text-gray-700">
+              Date
+            </label>
             <Popover>
               <PopoverTrigger asChild>
                 <button className="justify-start flex items-center text-left font-normal w-full p-2 border rounded-md">
@@ -157,7 +187,8 @@ const EditNoteForm = ({ note }) => {
                   {date?.from ? (
                     date.to ? (
                       <>
-                        {format(date.from, "LLL dd, y")} - {format(date.to, "LLL dd, y")}
+                        {format(date.from, "LLL dd, y")} -{" "}
+                        {format(date.to, "LLL dd, y")}
                       </>
                     ) : (
                       format(date.from, "LLL dd, y")
@@ -184,48 +215,64 @@ const EditNoteForm = ({ note }) => {
                 />
               </PopoverContent>
             </Popover>
-            {errors.date && <p className="text-red-600 text-sm">{errors.date.message}</p>}
+            {errors.date && (
+              <p className="text-red-600 text-sm">{errors.date.message}</p>
+            )}
           </div>
         </div>
 
         <div className="space-y-4">
           <div>
-            <label className="block text-sm font-medium text-gray-700">Subject</label>
+            <label className="block text-sm font-medium text-gray-700">
+              Subject
+            </label>
             <input
               type="text"
               {...register("subject")}
               className="mt-1 block w-full p-2 border rounded-md"
               placeholder="e.g., Science"
             />
-            {errors.subject && <p className="text-red-600 text-sm">{errors.subject.message}</p>}
+            {errors.subject && (
+              <p className="text-red-600 text-sm">{errors.subject.message}</p>
+            )}
           </div>
 
           <div>
-            <label className="block text-sm font-medium text-gray-700">Topic</label>
+            <label className="block text-sm font-medium text-gray-700">
+              Topic
+            </label>
             <input
               type="text"
               {...register("topic")}
               className="mt-1 block w-full p-2 border rounded-md"
               placeholder="e.g., Plants and Their Growth"
             />
-            {errors.topic && <p className="text-red-600 text-sm">{errors.topic.message}</p>}
+            {errors.topic && (
+              <p className="text-red-600 text-sm">{errors.topic.message}</p>
+            )}
           </div>
 
           <div>
-            <label className="block text-sm font-medium text-gray-700">Sub-Topic</label>
+            <label className="block text-sm font-medium text-gray-700">
+              Sub-Topic
+            </label>
             <input
               type="text"
               {...register("sub_topic")}
               className="mt-1 block w-full p-2 border rounded-md"
               placeholder="e.g., Photosynthesis"
             />
-            {errors.sub_topic && <p className="text-red-600 text-sm">{errors.sub_topic.message}</p>}
+            {errors.sub_topic && (
+              <p className="text-red-600 text-sm">{errors.sub_topic.message}</p>
+            )}
           </div>
         </div>
 
         <div className="flex sm:flex-row flex-col sm:space-x-4">
           <div className="sm:flex-1">
-            <label className="block text-sm font-medium text-gray-700">Class</label>
+            <label className="block text-sm font-medium text-gray-700">
+              Class
+            </label>
             <select
               {...register("class")}
               onChange={handleClassChange}
@@ -238,11 +285,15 @@ const EditNoteForm = ({ note }) => {
                 </option>
               ))}
             </select>
-            {errors.class && <p className="text-red-600 text-sm">{errors.class.message}</p>}
+            {errors.class && (
+              <p className="text-red-600 text-sm">{errors.class.message}</p>
+            )}
           </div>
 
           <div className="sm:flex-1">
-            <label className="block text-sm font-medium text-gray-700">Arm</label>
+            <label className="block text-sm font-medium text-gray-700">
+              Arm
+            </label>
             <select
               {...register("arm")}
               className="mt-1 block w-full p-2 border rounded-md"
@@ -254,52 +305,74 @@ const EditNoteForm = ({ note }) => {
                 </option>
               ))}
             </select>
-            {errors.arm && <p className="text-red-600 text-sm">{errors.arm.message}</p>}
+            {errors.arm && (
+              <p className="text-red-600 text-sm">{errors.arm.message}</p>
+            )}
           </div>
         </div>
 
         <div className="flex sm:flex-row flex-col sm:space-x-4">
           <div className="sm:flex-1">
-            <label className="block text-sm font-medium text-gray-700">Duration</label>
+            <label className="block text-sm font-medium text-gray-700">
+              Duration
+            </label>
             <input
               type="text"
               {...register("duration")}
               className="mt-1 block w-full p-2 border rounded-md"
               placeholder="e.g., 45 Minutes"
             />
-            {errors.duration && <p className="text-red-600 text-sm">{errors.duration.message}</p>}
+            {errors.duration && (
+              <p className="text-red-600 text-sm">{errors.duration.message}</p>
+            )}
           </div>
 
           <div className="sm:flex-1">
-            <label className="block text-sm font-medium text-gray-700">Age Group</label>
+            <label className="block text-sm font-medium text-gray-700">
+              Age Group
+            </label>
             <input
               type="text"
               {...register("age_group")}
               className="mt-1 block w-full p-2 border rounded-md"
               placeholder="e.g., 10-12 Years"
             />
-            {errors.age_group && <p className="text-red-600 text-sm">{errors.age_group.message}</p>}
+            {errors.age_group && (
+              <p className="text-red-600 text-sm">{errors.age_group.message}</p>
+            )}
           </div>
         </div>
 
         <div>
-          <label className="block text-sm font-medium text-gray-700">Instructional Objectives</label>
+          <label className="block text-sm font-medium text-gray-700">
+            Instructional Objectives
+          </label>
           <textarea
             {...register("objectives.0")}
             className="mt-1 block w-full p-2 border rounded-md"
             placeholder="List your instructional objectives here..."
           />
-          {errors.objectives && <p className="text-red-600 text-sm">{errors.objectives[0]?.message}</p>}
+          {errors.objectives && (
+            <p className="text-red-600 text-sm">
+              {errors.objectives[0]?.message}
+            </p>
+          )}
         </div>
 
         <div>
-          <label className="block text-sm font-medium text-gray-700">Instructional Resources</label>
+          <label className="block text-sm font-medium text-gray-700">
+            Instructional Resources
+          </label>
           <textarea
             {...register("resources.0")}
             className="mt-1 block w-full p-2 border rounded-md"
             placeholder="List your instructional resources here..."
           />
-          {errors.resources && <p className="text-red-600 text-sm">{errors.resources[0]?.message}</p>}
+          {errors.resources && (
+            <p className="text-red-600 text-sm">
+              {errors.resources[0]?.message}
+            </p>
+          )}
         </div>
 
         {["step1", "step2", "step3", "step4", "step5"].map((step, index) =>
@@ -307,29 +380,37 @@ const EditNoteForm = ({ note }) => {
         )}
 
         <div>
-          <label className="block text-sm font-medium text-gray-700">Assignment</label>
+          <label className="block text-sm font-medium text-gray-700">
+            Assignment
+          </label>
           <textarea
             {...register("assignment")}
             className="mt-1 block w-full p-2 border rounded-md"
             placeholder="Assignment details"
           />
-          {errors.assignment && <p className="text-red-600 text-sm">{errors.assignment.message}</p>}
+          {errors.assignment && (
+            <p className="text-red-600 text-sm">{errors.assignment.message}</p>
+          )}
         </div>
 
         <div>
-          <label className="block text-sm font-medium text-gray-700">Reference</label>
+          <label className="block text-sm font-medium text-gray-700">
+            Reference
+          </label>
           <textarea
             {...register("reference")}
             className="mt-1 block w-full p-2 border rounded-md"
             placeholder="Reference details"
           />
-          {errors.reference && <p className="text-red-600 text-sm">{errors.reference.message}</p>}
+          {errors.reference && (
+            <p className="text-red-600 text-sm">{errors.reference.message}</p>
+          )}
         </div>
 
         <div>
           <button
             type="submit"
-             className="w-full py-2 px-4 bg-red-1 hover:bg-red-500 text-white font-semibold rounded-md"
+            className="w-full py-2 px-4 bg-red-1 hover:bg-red-500 text-white font-semibold rounded-md"
             disabled={loading}
           >
             {loading ? "Updating..." : "Update Lesson Plan"}
