bea3ad98b0a160ad3476b883643d51ed2aae2e1b
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2024-09-12 22:18:25 +0100

    note

--- src/components/shared/RightSidebar.tsx
+++ src/components/shared/RightSidebar.tsx
@@ -2,11 +2,30 @@ import { useAuth } from "@/contexts/AuthContext";
 import styles from "@/styles/login.module.css";
 import { BookText } from "lucide-react";
 import { Calendar } from "@/components/ui/calendar";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { axiosPrivate } from "@/api/axios";
+import { Watch } from "react-loader-spinner";
 
 function RightSidebar() {
   const { user, role } = useAuth();
   const [date, setDate] = useState<Date | undefined>(new Date());
+  const [notes, setNotes] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const getAllNotes = async () => {
+    setLoading(true); // Set loading to true before fetching data
+    try {
+      const response = await axiosPrivate.get("/all-notes");
+      setNotes(response.data.notes);
+    } catch (error) {
+      console.error("Error fetching notes:", error);
+    } finally {
+      setLoading(false); // Set loading to false once data is fetched or if an error occurs
+    }
+  };
+  useEffect(() => {
+    getAllNotes();
+  }, [axiosPrivate]);
+  console.log(notes);
   return (
     <>
       {role === "Teacher" ? (
@@ -37,7 +56,51 @@ function RightSidebar() {
           </div>
         </section>
       ) : (
-        <section className="custom-scrollbar rightsidebar"></section>
+        <section className="custom-scrollbar rightsidebar">
+          <div className="">
+            {loading ? (
+              <div className="flex justify-center items-center h-64">
+                <Watch
+                  height="80"
+                  width="80"
+                  radius="48"
+                  color="#B43330"
+                  ariaLabel="watch-loading"
+                  wrapperStyle={{}}
+                  wrapperClass=""
+                />
+              </div>
+            ) : (
+              <>
+                <h4>Upcoming Notes</h4>
+                <div className={styles.container}>
+                  {notes.map((note) => (
+                    <div
+                      key={note.id}
+                      className={styles.box}
+                      style={{ "--clr": "#fc5f9b" }}
+                    >
+                      <div className={styles.content}>
+                        <div className={styles.icon}>
+                          <BookText />
+                        </div>
+                        <div className={styles.text}>
+                          <h3>{note.subject}</h3>
+                          <p>
+                            By {note?.user?.firstname} {note?.user?.lastname}
+                          </p>
+                        </div>
+                        <div className="absolute text-tiny-medium text-light-2 bottom-2 right-1">
+                          <p>{new Date(note.created_at).toLocaleDateString()}</p>
+                        </div>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              </>
+            )}
+          </div>
+        </section>
       )}
     </>
   );
