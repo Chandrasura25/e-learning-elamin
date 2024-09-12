4537b3c8ac66120d6d8da3ae7a2353d0695044ff
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2024-09-12 13:33:17 +0100

    note creation

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,13 +1,38 @@
+import { axiosPrivate } from '@/api/axios';
 import DashboardTable from '@/components/Tables/DashboardTable';
 import { useAuth } from '@/contexts/AuthContext';
+import { useEffect, useState } from 'react';
+import { Watch } from "react-loader-spinner";
+
 const Dashboard = () => {
+  const [notes, setNotes] = useState([]);
+  const [loading, setLoading] = useState(true); // Add loading state
   const { user } = useAuth();
+
+  useEffect(() => {
+    const getNotes = async () => {
+      try {
+        if (user?.id) {
+          setLoading(true); // Set loading to true before fetching data
+          const response = await axiosPrivate.get(`/user/${user.id}`);
+          setNotes(response.data.notes); // Assuming the API returns an object of notes
+        }
+      } catch (error) {
+        console.error("Error fetching notes:", error);
+      } finally {
+        setLoading(false); // Set loading to false once data is fetched or if an error occurs
+      }
+    };
+
+    getNotes();
+  }, [user?.id, axiosPrivate]);
+
   return (
     <div>
       <div className="w-full h-fit py-3 px-8 flex justify-between items-center bg-white rounded-[16px]">
         <div className="">
           <h2 className='text-heading2-semibold text-light-4'>Hi {user?.firstname}!</h2>
-          <p>You have completed Lorem, ipsum dolor. Start writing your E-note</p>
+          <p>Make sure to jot down a note about today’s lesson and any important details for the students!</p>
         </div>
         <div className="">
           <img src="/images/writing.gif" alt="" className='h-24' />
@@ -15,10 +40,24 @@ const Dashboard = () => {
         <div className=""></div>
       </div>
       <div className="bg-white mt-4 p-4 rounded-[16px]">
-        <DashboardTable />
+        {loading ? (
+          <div className="flex justify-center items-center h-64">
+            <Watch
+              height="80"
+              width="80"
+              radius="48"
+              color="#B43330"
+              ariaLabel="watch-loading"
+              wrapperStyle={{}}
+              wrapperClass=""
+            />
+          </div>
+        ) : (
+          <DashboardTable notes={notes} />
+        )}
       </div>
     </div>
   )
 }
 
-export default Dashboard
+export default Dashboard;
