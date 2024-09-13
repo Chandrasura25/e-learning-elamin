158621946cc9313a32ff61dbacbb952870d5a9df
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2024-09-13 01:51:41 +0100

    note

--- src/pages/superadmin/ReviewNote.tsx
+++ src/pages/superadmin/ReviewNote.tsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import { axiosPrivate } from "@/api/axios";
+import EditNoteForm from "@/components/forms/EditNoteForm";
+import { useState, useEffect } from "react";
+import { Watch } from "react-loader-spinner";
+import { useParams } from "react-router-dom";
 
 const ReviewNote = () => {
+  const { id } = useParams();
+  const [plan, setPlan] = useState({});
+  const [loading, setLoading] = useState(false);
+  const getNote = async () => {
+    try {
+      if (id) {
+        setLoading(true); // Set loading to true before fetching data
+        const response = await axiosPrivate.get(`/note/${id}`);
+        setPlan(response.data.lesson_plan); // Assuming the API returns an object of notes
+      }
+    } catch (error) {
+      console.error("Error fetching notes:", error);
+    } finally {
+      setLoading(false); // Set loading to false once data is fetched or if an error occurs
+    }
+  };
+  useEffect(() => {
+    getNote();
+  }, [id, axiosPrivate]);
   return (
-    <div>ReviewNote</div>
-  )
-}
+    <div>
+      {loading ? (
+        <div className="flex justify-center items-center h-64">
+          <Watch
+            height="80"
+            width="80"
+            radius="48"
+            color="#B43330"
+            ariaLabel="watch-loading"
+            wrapperStyle={{}}
+            wrapperClass=""
+          />
+        </div>
+      ) : (
+        <EditNoteForm note={plan} />
+      )}
+    </div>
+  );
+};
 
-export default ReviewNote
+export default ReviewNote;
