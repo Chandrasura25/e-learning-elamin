77ffc99c2ab0afc9a5565891d8b02d4980a50e26
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2024-09-16 11:25:02 +0100

    changes

--- src/pages/CreateNote.tsx
+++ src/pages/CreateNote.tsx
@@ -1,10 +1,11 @@
+import { CreateForm } from '@/components/forms/CreateForm';
 import { LessonPlanForm } from '@/components/forms/LessonPlanForm';
 
 const CreateNote = () => {
   return (
     <div>
         <div className="">
-            <LessonPlanForm />
+            <CreateForm />
         </div>
     </div>
   )
